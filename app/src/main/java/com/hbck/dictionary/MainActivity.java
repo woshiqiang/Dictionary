ff012123fa820561f84abe322acc571cad209385
woshiqiang <1227650311@qq.com>
2018-11-21 17:41:20 +0800

    Initial commit

---
+++ app/src/main/java/com/hbck/dictionary/MainActivity.java
@@ -0,0 +1,54 @@
+package com.hbck.dictionary;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et_content;
+    private TextView tv_result;
+//    http://fanyi.youdao.com/openapi.do?keyfrom=youdao111&key=60638690&type=data&doctype=json&version=1.1&q=%E5%AD%97%E5%85%B8
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    public void onClick(View view) {
+        submit();
+    }
+
+    private void initView() {
+        et_content = (EditText) findViewById(R.id.et_content);
+        tv_result = (TextView) findViewById(R.id.tv_result);
+    }
+
+    private void submit() {
+        String content = et_content.getText().toString().trim();
+        if (TextUtils.isEmpty(content)) {
+            Toast.makeText(this, "关键词", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SearchTask task = new SearchTask(handler.obtainMessage(11));
+        task.execute(content);
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            tv_result.setText(msg.obj.toString());
+        }
+    };
+}
---
+++ app/src/main/java/com/hbck/dictionary/SearchTask.java
@@ -0,0 +1,86 @@
+package com.hbck.dictionary;
+
+import android.os.AsyncTask;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @Date 2018-11-21.
+ */
+public class SearchTask extends AsyncTask<String, String, String> {
+    public static final String BASE_URL = "http://fanyi.youdao.com/openapi.do?keyfrom=youdao111&key=60638690&type=data&doctype=json&version=1.1&q=";
+    private static final String TAG = "SearchTask";
+    private Message msg;
+
+    public SearchTask(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String result = "";
+        try {
+            // 新建一个URL对象
+            URL url = new URL(BASE_URL + strings[0]);
+            // 打开一个HttpURLConnection连接
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            // 设置连接主机超时时间
+            urlConn.setConnectTimeout(5 * 1000);
+            // 设置为GET请求
+            urlConn.setRequestMethod("GET");
+            // 开始连接
+            urlConn.connect();
+
+            // 判断请求是否成功
+            if (urlConn.getResponseCode() == 200) {
+                // 获取返回的数据
+                result = streamToString(urlConn.getInputStream());
+                Log.e(TAG, "Get方式请求成功，result--->" + result);
+            } else {
+                Log.e(TAG, "Get方式请求失败");
+            }
+            // 关闭连接
+            urlConn.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        msg.obj = s;
+        msg.sendToTarget();
+    }
+
+    /**
+     * 将输入流转换成字符串
+     *
+     * @param is 从网络获取的输入流
+     * @return
+     */
+    public String streamToString(InputStream is) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            baos.close();
+            is.close();
+            byte[] byteArray = baos.toByteArray();
+            return new String(byteArray);
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+            return null;
+        }
+    }
+}
